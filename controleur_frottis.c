f7b543e71fa6da4a8b3684e67dad8b3b543a7fef
heyKid0 <109003488+heyKid0@users.noreply.github.com>
2022-11-22 15:09:31 +0100

    V2 Controleur factifs de frottis avec Buzzer
    Porbleme de MCU --> Wemos D1 mini

--- controleur_frottis.c
+++ controleur_frottis.c
@@ -1,11 +1,17 @@
+
+
 /*
 Conception d'un controleur factifs de frottis
 
 Objectif    : Piloter l'indication de contamination à distance par une telecommande
 Cadre       : Pedagogique
-Harware     : Arduino nano / Leds (rouge & verte) / Interupteur 
+Harware     : Arduino nano / Leds (rouge & verte) / Interupteur / Buzzer actif 
 
-modifie 29 septembre 2022
+modifie 10 novembre 2022
+
+Pour la v2 : on utilise un Wemos D1 mini (probleme de recetption de MCU a Orano)
+Normalement on utilise une Arduino nano, cela explique les changements de Pin
+
 */
 
 
@@ -15,14 +21,16 @@ modifie 29 septembre 2022
  
 
 //On defini les pins LEDs et capteur IR
-const int RECV_PIN  = 4;
-const int redPin    = 7; 
-const int greenPin  = 6;
+const int RECV_PIN  = 5;
+const int redPin    = 0; 
+const int greenPin  = 4;
+const int buzz      = 2;
 
 //On defini nos variables 
 int togglestate1    = 0;   
 int togglestate2    = 0;
 int compteur;
+
 
 // On defini le recepteur IR et les objets de resultats
 IRrecv irrecv(RECV_PIN);
@@ -34,8 +42,9 @@ void setup(){
   // On setup les LEDs en OUTPUT
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
+  pinMode(buzz,OUTPUT);
 
-  blink(); // Foction pour indiuqer la mise en place du systeme 
+  synchronisation(); // Foction pour indiuqer la mise en place du systeme 
 
 }
  
@@ -44,29 +53,37 @@ void loop(){
     if (irrecv.decode(&results)){
 
         switch(results.value){
-          case 0xFF18E7: //Button Fleche du haut
-        // Toggle LED On or Off
-        if(togglestate1==0){
-        digitalWrite(redPin, HIGH);
-        togglestate1=1;
-        }
-        else {
-        digitalWrite(redPin, LOW);
-        togglestate1=0;
-        }
-        break;
-      
-          case 0xFF4AB5: //Button flecge du bas
-        // Toggle LED On or Off
+        
+        case 0xFF18E7:        //Button flecge du haut | LED vert
+
         if(togglestate2==0){
         digitalWrite(greenPin, HIGH);
+        delay(250);
         togglestate2=1;
         }
         else {
         digitalWrite(greenPin, LOW);
+        delay(250);
         togglestate2=0;
         }
         break;
+
+        case 0xFF4AB5:        //Button Fleche du bas | LED rouge + buzzer 
+      
+        if(togglestate1==0){
+        digitalWrite(redPin, HIGH);
+        digitalWrite(buzz,HIGH);
+        delay(250);
+        togglestate1=1;
+        }
+        else {
+        digitalWrite(redPin, LOW);
+        digitalWrite(buzz,LOW);
+        delay(250);
+        togglestate1=0;
+        }
+        break;
+         
         
     }
     irrecv.resume(); 
@@ -75,15 +92,19 @@ void loop(){
 
 ///// Fonction  /////
 
-void blink()
+void synchronisation()
 {
 for (compteur = 0; compteur < 5; compteur ++)
   {
-    digitalWrite (redPin,HIGH);
-    digitalWrite (greenPin,HIGH);
+    digitalWrite    (redPin,HIGH);
+    digitalWrite    (greenPin,HIGH);
+    digitalWrite    (buzz,HIGH);
     delay(200);
-    digitalWrite (redPin,LOW);
-    digitalWrite (greenPin,LOW);
+    digitalWrite    (redPin,LOW);
+    digitalWrite    (greenPin,LOW);
+    digitalWrite    (buzz,LOW);
     delay(150);
   }
 }
+
+
